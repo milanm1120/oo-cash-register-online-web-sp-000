555317d155f983e7b9b3270e329436a1777b33f1
Milan Mody <milanm1120@gmail.com>
2021-03-15 01:12:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(total, price, quantity = 1)        #set optional quantity for a minimum of 1
+    @total += price * quantity                    #minima default quantity of 1, aditional quantity will be added to the price.
+  end
+
+  def apply_discount
+    if @discount > 0
+      @total = (@total - @total * @discount.to_f / 100)               #to_f makes it a floating number
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def items
+    
+  end
+
+
+end
+
+
+cash_register = CashRegister.new
